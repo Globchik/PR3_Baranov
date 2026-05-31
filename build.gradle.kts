f055c33ebac64f042d26ce26da6cddb4b57b6556
Artem Baranov <artem.baranov@nure.ua>
2026-05-31 20:00:47 +0300

    add deps and permissions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+implementation("com.squareup.retrofit2:retrofit:2.9.0")
+implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+implementation("com.google.code.gson:gson:2.10.1")
+
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+implementation("androidx.compose.ui:ui")
+implementation("androidx.compose.material3:material3")
